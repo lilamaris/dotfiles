45d1d56a002fd172bf669e79b510ad78ffc4deba
lilamaris <lilamaris.dev@gmail.com>
2026-08-19 14:46:24 +0000

    feat: add nvim blink.cmp

---
+++ home/nvim/.config/nvim/lua/plugins/editor/blink-cmp.lua
@@ -0,0 +1,13 @@
+return {
+  'saghen/blink.cmp',
+  opts = {
+    sources = {
+      default = { 'lsp' },
+    },
+    completion = {
+      documentation = {
+        auto_show = true,
+      },
+    },
+  },
+}
