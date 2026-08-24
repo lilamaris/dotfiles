b3de77c558460a85d92b58188582a1ae1b44b5bc
lilamaris <lilamaris.dev@gmail.com>
2026-08-25 03:57:51 +0900

    feat: add qs autostart

--- home/hypr/.config/hypr/autostart.lua
+++ home/hypr/.config/hypr/autostart.lua
@@ -1,5 +1,4 @@
 hl.on("hyprland.start", function()
-  hl.exec_cmd("fcitx5-remove -r & fcitx5 -d --replace & fcitx5-remote -r")
-  hl.exec_cmd("awww-daemon")
-end
-)
+	hl.exec_cmd("fcitx5-remove -r & fcitx5 -d --replace & fcitx5-remote -r")
+	hl.exec_cmd("qs & awww-daemon")
+end)
